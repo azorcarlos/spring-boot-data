ca92a1899e8804941e62816f52754c887c5e4cba
Azor Carlos <azorcarlos@gmail.com>
2020-10-06 19:43:11 -0300

    Start version

---
+++ src/main/java/com/azorcarlos/Data/DataApplication.java
@@ -0,0 +1,13 @@
+package com.azorcarlos.Data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataApplication.class, args);
+	}
+
+}
